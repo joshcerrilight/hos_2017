d2277d623d9b0f1aef6b02d38e8537c5e93703d4
MessyMilfD <joshcerrilight@gmail.com>
2017-07-15 02:50:19 -0400

    Added single channel pwMUD w/serial output.

---
+++ _1ch_pw_MUD/_1ch_pw_MUD.ino
@@ -0,0 +1,36 @@
+//Setup it up
+void setup(){
+Serial.begin(9600);
+}
+
+//Assign those pins
+const int dim1=A0;
+const int ch1=3;
+
+
+int dim1Intensity = 0;
+
+// Loop it
+void loop(){
+
+//Assign them variables
+int dim1read = analogRead(dim1);
+int ch1pwm = map(dim1read, 0, 995, 255, 10);
+int ch1Intensity=map(ch1pwm, 10, 255,1, 100); 
+if(dim1read != dim1Intensity){
+	dim1Intensity = dim1read;
+	analogWrite(ch1, ch1pwm);
+		}
+int ch1info[]={
+dim1Intensity, ch1pwm, ch1Intensity
+		};
+Serial.println("Dimmer 1, Ch1 PWM, Ch1 Intensity");
+
+int i;
+for(i = 0;i<3;i=i+1){
+	Serial.println(ch1info[i]);
+		};
+
+delay(30);
+		
+}
