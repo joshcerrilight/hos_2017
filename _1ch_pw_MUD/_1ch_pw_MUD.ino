f47303bc8bcd46b716c66255b3404c015ba99a85
MessyMilfD <joshcerrilight@gmail.com>
2017-07-17 14:33:32 -0400

    Updated pin numbers removed serial functions.

--- _1ch_pw_MUD/_1ch_pw_MUD.ino
+++ _1ch_pw_MUD/_1ch_pw_MUD.ino
@@ -1,11 +1,11 @@
 //Setup it up
 void setup(){
-Serial.begin(9600);
+
 }
 
 //Assign those pins
-const int dim1=5;
-const int ch1=0;
+const int dim1=A1;
+const int ch1=4;
 
 
 int dim1Intensity = 0;
@@ -15,21 +15,11 @@ void loop(){
 
 //Assign them variables
 int dim1read = analogRead(dim1);
-int ch1pwm = map(dim1read, 0, 995, 255, 10);
-int ch1Intensity=map(ch1pwm, 10, 255,1, 100); 
+int ch1pwm = map(dim1read, 0, 1023, 0, 255);
 if(dim1read != dim1Intensity){
 	dim1Intensity = dim1read;
 	analogWrite(ch1, ch1pwm);
 		}
-int ch1info[]={
-dim1Intensity, ch1pwm, ch1Intensity
-		};
-Serial.println("Dimmer 1, Ch1 PWM, Ch1 Intensity");
-
-int i;
-for(i = 0;i<3;i=i+1){
-	Serial.println(ch1info[i]);
-		};
 
 delay(30);
 		
